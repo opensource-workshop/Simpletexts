1d99e16bba369432bb42e5d122e17bd0950d0337
Mitsuru Mutaguchi <akagane99@gmail.com>
2016-12-26 16:54:49 +0900

    コメント記載 - Model/SimpletextsAppModel

--- Model/SimpletextsAppModel.php
+++ Model/SimpletextsAppModel.php
@@ -12,6 +12,11 @@ App::uses('AppModel', 'Model');
 /**
  * Summary for Simpletexts AppModel
  *
+ * [Cakephpの決まり] XxxxAppModelは、プラグインのModelの親クラス
+ *
+ * [phpstrom用] ＠property にmodel, Component等を記載すると、子クラスで記載したclassをソースコード上にリンクして
+ * 開く事ができる
+ *
  * @property SimpletextSetting $SimpletextSetting
  * @property SimpletextFrameSetting $SimpletextFrameSetting
  */
