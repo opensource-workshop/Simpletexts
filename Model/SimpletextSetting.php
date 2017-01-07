fe192c5a0e30bc06965ca18ae3f419a413550cf0
Mitsuru Mutaguchi <akagane99@gmail.com>
2017-01-08 07:09:09 +0900

    コメント追記 - Model/SimpletextSetting

--- Model/SimpletextSetting.php
+++ Model/SimpletextSetting.php
@@ -16,6 +16,11 @@ App::uses('BlockBaseModel', 'Blocks.Model');
  * SimpletextSetting Model
  *
  * [NetCommonsプラグイン] XxxxxSettingは他プラグインからほぼコピペ
+ * [NetCommons独自] XxxxxSettingはBlockBaseModelを継承する
+ * Plugin\Blocks\Model\BlockBaseModel.php
+ *   XxxxxSettingは通常、対応するテーブルがない。
+ *   そのため、BlockBaseModelでは、テーブルを使わない（$useTable = false;） が初期値になっており、
+ *   save()も上書きされていて、テーブルを使わない場合の処理が拡張されている。
  */
 class SimpletextSetting extends BlockBaseModel {
 
@@ -23,6 +28,8 @@ class SimpletextSetting extends BlockBaseModel {
  * Custom database table name
  *
  * [Cakephoの決まり] テーブルのないモデルは、falseを指定する
+ * [NetCommonsプラグイン] BlockBaseModelで $useTable = false; なので本来不要
+ * 後から BlockBaseModel つけたした経緯から、残っているものと思われる
  *
  * @var string
  */
@@ -32,17 +39,27 @@ class SimpletextSetting extends BlockBaseModel {
  * Validation rules
  *
  * @var array
+ * @see Simpletext::$validate と説明同様
+ * [NetCommons独自] XxxxxSettingは $actsAs の BlockSettingBehavior を使う事が必須で、
+ * BlockSettingBehavior::beforeValidate() で validateが動くので、このクラスでvalidate定義は不要。
  */
 	public $validate = array();
 
 /**
- * use behaviors
+ * [Cakephpの決まり] use behaviors
  *
  * @var array
  */
 	public $actsAs = array(
+		// [NetCommons独自]
 		'Blocks.BlockRolePermission',
+		// [NetCommons独自] ブロック設定（縦持ちデータ）のcreate, save, deleteのファンクションや
+		// 自動的にvalidateするbeforeValidateを提供
+		// Plugin\Blocks\Model\Behavior\BlockSettingBehavior.php
+		// https://netcommons3.github.io/NetCommons3Docs/phpdoc/Blocks/classes/BlockSettingBehavior.html
 		'Blocks.BlockSetting' => array(
+			// 利用するブロック設定
+			// BlockSettingBehaviorの定数も利用できるし、独自のStringで設定する事もできる。
 			BlockSettingBehavior::FIELD_USE_WORKFLOW,
 		),
 	);
